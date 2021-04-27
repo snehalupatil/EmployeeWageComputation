f834487ca9a39b2582d8af9e740e5536dbfbbf17
snehalupatil <patilsnehal4575@gmail.com>
2021-04-27 11:35:09 +0530

    Added: Welcome message added to Employee Wage

---
+++ WelcomeToEmpWage.java
@@ -0,0 +1,10 @@
+
+public class WelcomeToEmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Employee Wage Computation");
+
+	}
+
+}
